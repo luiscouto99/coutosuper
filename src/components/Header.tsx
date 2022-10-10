3cc3f102d112bcee11ab7d0a46ed92d7f28ec28b
Luis Couto <luis.couto@mindswap.academy>
2022-10-10 12:00:53 +0100

    proj structure and header

---
+++ src/components/Header.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import logo from '../assets/logo.jpg'
+
+import styled from 'styled-components'
+
+const HeaderContainer = styled.header`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin: 20px 160px;
+`;
+
+const LogoLink = styled(Link)`
+    width: 100px;
+`;
+
+const Logo = styled.img`
+    width: 100%;
+`;
+
+const NavbarContainer = styled.nav`
+    display: flex;
+    gap: 20px;
+`;
+
+const NavbarLink = styled(Link)`
+    font-weight: 500;
+`;
+
+const Header = () => {
+    return (
+        <HeaderContainer>
+            <LogoLink to='/'>
+                <Logo src={logo} alt="Logótipo Supermercado Couto" />
+            </LogoLink>
+
+            <NavbarContainer>
+                <NavbarLink to='/'>Bem Vindo</NavbarLink>
+                <NavbarLink to='/sobre-nos'>Sobre Nós</NavbarLink>
+                <NavbarLink to='/produtos'>Produtos</NavbarLink>
+                <NavbarLink to='/promocoes'>Promoções</NavbarLink>
+                <NavbarLink to='/contactos'>Contactos</NavbarLink>
+            </NavbarContainer>
+
+        </HeaderContainer>
+    )
+}
+
+export default Header
