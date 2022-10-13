f60de7ff9181719f2bf8e73216c19192f986481f
Luis Couto <luis.couto@mindswap.academy>
2022-10-13 18:47:30 +0100

    footer responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -27,6 +27,14 @@ const FooterContainer = styled.footer`
     hsla(217, 99%, 29%, 1) 0%,
     hsla(156, 100%, 26%, 1) 100%
   );
+
+  @media (max-width: 1410px) {
+    flex-direction: column;
+  }
+
+  @media (max-width: 992px) {
+    padding: 40px 60px;
+  }
 `;
 
 const FooterInfo = styled.div`
@@ -45,6 +53,10 @@ const Title = styled.h2`
   margin-bottom: 12px;
   font-size: 24px;
   color: white;
+
+  @media (max-width: 1476px) {
+    font-size: 20px;
+  }
 `;
 
 const OptionItem = styled.li`
@@ -52,6 +64,15 @@ const OptionItem = styled.li`
   align-items: center;
   font-size: 18px;
   margin-bottom: 18px;
+
+  @media (max-width: 1476px) {
+    font-size: 16px;
+  }
+
+  @media (max-width: 688px) {
+    flex-direction: column;
+    align-items: flex-start
+  }
 `;
 
 const Option = styled.p`
@@ -76,6 +97,28 @@ const MapContainer = styled.div`
 const Map = styled.iframe`
   border: 0;
   border-radius: 10px;
+  width: 800px;
+  height: 450px;
+
+  @media (max-width: 1876px) {
+    width: 600px;
+  }
+
+  @media (max-width: 1676px) {
+    width: 400px;
+  }
+
+  @media (max-width: 1410px) {
+    width: 528px;
+  }
+
+  @media (max-width: 688px) {
+    width: 462px;
+  }
+
+  @media (max-width: 599px) {
+    width: 380px;
+  }
 `;
 
 const Footer = () => {
@@ -124,8 +167,6 @@ const Footer = () => {
       <MapContainer>
         <Map
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.335500578782!2d-8.578320267910078!3d40.754645076830094!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd239b87160b7a7d%3A0xf3ce82883ba30487!2sCouto%20-%20Food%20Products%20Markets%2C%20Ltd.!5e0!3m2!1sen!2spt!4v1665504548197!5m2!1sen!2spt"
-          width="800"
-          height="450"
           loading="lazy"
         />
       </MapContainer>
